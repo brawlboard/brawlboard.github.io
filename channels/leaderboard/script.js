3df2c66803ba0419e2eb68a87f7ac95bcb96aa3b
ytstats <nazmulkabir2666@gmail.com>
2022-05-15 22:52:15 +0600

    changed api

--- channels/leaderboard/script.js
+++ channels/leaderboard/script.js
@@ -1,31 +1,34 @@
 let CONFIG = {
-    "numberOfChannels": 24,
+    "numberOfChannels": 22,
     "numberOfRows": 2
 };
-let CHANNELID = [
-    'UCThfpuJ8LX7YeRpmSvB_lIQ',
-    'UCBGjyYxljtz9dRdccMnkDbQ',
-    'UCR1RhHf1ON0eDyi2dN3rXNA',
-    'UCVuRmP-R6Fx8h0xRXsM3zog',
-    'UCzuzyWrqkhonYsuMSEDhPKA',
-    'UCrxecZ-Gxay6lZDVQlaDcnQ',
-    'UCyBdMMfbBbJy-YMMT8PJHtA',
-    'UCpmhVItCsN4AbBFtc7sk_Rw',
-    'UCuDBUm5jiUI790e4pmgOyxg',
-    'UChfJ5KDAmcMyA-sg61pm8oQ',
-    'UCv05pOTj753Akezl4bk5V6g',
-    'UCdDEtGAW0_BPtgmdSzz6MOA',
-    'UCMlULIKTHv9MCWdaKgH712Q',
-    'UC8rcDovVw8stUqK47105MMQ',
-    'UCHkbIdjKbk14EIkMcRoqs3g',
-    'UCAKyXT0Vot7GHxtWKOEe4cg',
-    'UCOYorfo-ueDgFg-6B24GJnQ',
-    'UCYWBtJ62J2RBARQJvRCQU4g',
-    'UCnIdnC__X7Ywaf5bl5wGMpA',
-    'UCrkgqEJL5Kd5gBBHL4wTG9Q',
-    'UC_s7d7w9BIZgAVwmzA7hBMw',
-    'UCz1Bnml93YeUmCT5BH0763g'
-];
+
+let CHANNELID = ['UCThfpuJ8LX7YeRpmSvB_lIQ',
+'UCBGjyYxljtz9dRdccMnkDbQ',
+'UCR1RhHf1ON0eDyi2dN3rXNA',
+'UCVuRmP-R6Fx8h0xRXsM3zog',
+'UCzuzyWrqkhonYsuMSEDhPKA',
+'UCrxecZ-Gxay6lZDVQlaDcnQ',
+'UCyBdMMfbBbJy-YMMT8PJHtA',
+'UCpmhVItCsN4AbBFtc7sk_Rw',
+'UCuDBUm5jiUI790e4pmgOyxg',
+'UChfJ5KDAmcMyA-sg61pm8oQ',
+'UCv05pOTj753Akezl4bk5V6g',
+'UCdDEtGAW0_BPtgmdSzz6MOA',
+'UCMlULIKTHv9MCWdaKgH712Q',
+'UC8rcDovVw8stUqK47105MMQ',
+'UCHkbIdjKbk14EIkMcRoqs3g',
+'UCAKyXT0Vot7GHxtWKOEe4cg',
+'UCOYorfo-ueDgFg-6B24GJnQ',
+'UCYWBtJ62J2RBARQJvRCQU4g',
+'UCnIdnC__X7Ywaf5bl5wGMpA',
+'UCrkgqEJL5Kd5gBBHL4wTG9Q',
+'UC_s7d7w9BIZgAVwmzA7hBMw',
+'UCz1Bnml93YeUmCT5BH0763g',
+'UCOYorfo-ueDgFg-6B24GJnQ',
+'UC3CeKXNtheO1jvomemi07FA'];
+
+
 let cData = []
 function arrayPush(item) {
     if (!cData.find(({ id }) => id === item.id)) {
@@ -36,38 +39,38 @@ function arrayPush(item) {
 
         var foundIndex = cData.findIndex(x => x.id == item.id);
         obj = cData[foundIndex];
-        obj.subcount = item.subcount;
+        obj.subcount = item.subcount || 0;
     }
 }
 async function updateData() {
-
+   
     for (var q = 0; q < CHANNELID.length; q++) {
         let id = CHANNELID[q];
-        fetch("https://api-v2.nextcounts.com/api/youtube/channel/estimate/mixerno/" + id, {
+        await fetch("https://mixerno.space/api/youtube-channel-counter/user/" + id, {
             method: "GET",
             mode: 'cors',
             headers: {
                 'Content-Type': 'application/json',
             },
         }).then(response => response.json()).then(result => {
+            if(parseInt(result.counts[0].count) === 0) return;
             let a = {
                 id: id,
-                subcount: parseInt(result.estimatedSubCount),
-                name: result.channelName,
-                logo: result.avatar
+                subcount: parseInt(result.counts[0].count) || 0,
+                name: result.user[0].count,
+                logo:result.user[1].count
             }
-            arrayPush(a);
+             arrayPush(a);
         });
     };
-    cData.sort((a, b) => parseFloat(b.subcount) - parseFloat(a.subcount));
-    console.log(cData)
 };
 
 async function updateCounter() {
 
+
     for (var q = 0; q < cData.length; q++) {
         let cId = cData[q].id;
-        
+        await cData.sort((a, b) => parseFloat(b.subcount) - parseFloat(a.subcount));
         cData.forEach(update);
         
         function update(item) {
@@ -84,4 +87,4 @@ async function updateCounter() {
 updateData();
 updateCounter();
 setInterval(updateData, 5 * 1000);
-setInterval(updateCounter, 3 * 1000);
+setInterval(updateCounter, 1 * 1000);
